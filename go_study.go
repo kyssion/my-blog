af187fcb6b49fb4e163c50bc63fca3c634b9768f
kys <1409915687@qq.com>
2018-04-30 18:07:25 +0800

    update

--- go_study.go
+++ go_study.go
@@ -7,21 +7,33 @@ import (
 )
 
 func main() {
-	r := RandInt(10, 13) //产生范围内随机数，防止编译器优化
-	fmt.Println(r, time.Now().UnixNano())
 	t := time.Now().UnixNano()
-	sum := 0
-	for i := 0; i <= 40000+r; i++ {
-		for j := 0; j <= 40000; j++ {
-			sum = sum + i*j
-		}
+	for i := 10000; i < 22222; i++ {
+		ddd(2, i)
 	}
 	i := float64(time.Now().UnixNano()-t) / (1000000 * 1000)
-	fmt.Println(sum)
 	fmt.Println(i)
 }
 
 func RandInt(min, max int) int {
 	rand.Seed(time.Now().UnixNano())
 	return rand.Intn(max-min) + min
+}
+func ddd(k int, n int) int {
+	// write your code here
+	sum := 0
+	for i := 0; i <= n; i++ {
+		num := i
+		for num/10 != 0 {
+			if num%10 == k {
+				sum++
+			}
+			num = num / 10
+		}
+		if num == k {
+			sum = sum + 1
+		}
+
+	}
+	return sum
 }
